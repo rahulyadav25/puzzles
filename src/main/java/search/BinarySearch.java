8527f6e7c3c4de6232cc6f953cd3896025b6482f
ishu <ishu@DESKTOP-UAPGL4U>
2017-09-17 19:25:16 -0400

    final commit binary search

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -1,5 +1,32 @@
 package search;
 
+import data.Data;
+
 public class BinarySearch {
 
+	public static void main(String[] args) {
+
+		BinarySearch bs = new BinarySearch();
+		System.out.println(bs.search(4));
+	}
+
+	public int search(int searchMe) {
+
+		int lower = 0;
+		int upper = Data.INT_ARRAY.length;
+		int mid = 0;
+
+		while (lower < upper) {
+			mid = ((upper - lower) / 2) + 1;
+
+			if (searchMe > Data.INT_ARRAY[mid]) {
+				lower = mid + 1;
+			} else if (searchMe < Data.INT_ARRAY[mid]) {
+				upper = mid - 1;
+			} else {
+				return Data.INT_ARRAY[mid];
+			}
+		}
+		return -1;
+	}
 }
