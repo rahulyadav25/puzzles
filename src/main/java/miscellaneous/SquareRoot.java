95e17b874440aec0f75ce00ded212fa2e86d79a4
ishu <ishu@DESKTOP-UAPGL4U>
2017-09-17 20:55:40 -0400

    square root impl

--- src/main/java/miscellaneous/SquareRoot.java
+++ src/main/java/miscellaneous/SquareRoot.java
@@ -5,20 +5,19 @@ public class SquareRoot {
 	public static void main(String[] args) {
 
 		SquareRoot sr = new SquareRoot();
-		System.out.println(sr.root(37));
+		System.out.println(sr.root(26));
 	}
 
 	private int root(int x) {
-		// Base cases
-		if (x == 0 || x == 1)
+		if (x == 0 || x == 1) {
 			return x;
+		}
 
-		// Staring from 1, try all numbers until
-		// i*i is greater than or equal to x.
-		int i = 1, result = 1;
+		int result = 1, i = 1;
 		while (result <= x) {
-			if (result == x)
+			if (result == x) {
 				return i;
+			}
 			i++;
 			result = i * i;
 		}
